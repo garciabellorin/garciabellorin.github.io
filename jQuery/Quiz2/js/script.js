4a25a893c18d633ac81a522a48a1ce6e82f3ebc0
Jose Garcia Bellorin <78644434+Dormeteus@users.noreply.github.com>
2021-09-14 10:42:17 -0500

    Adding Style

---
+++ jQuery/Quiz2/js/script.js
@@ -0,0 +1,13 @@
+// code fragment 1
+// $("li").each(function (idx, e) {
+//     $(e).css("color", "yellow");
+// });
+// //code fragment 2
+// $("li").each(function () {
+//     $(this).css("color", "yellow");
+// });
+//code fragment 3
+
+// $("li").each(function (idx) {
+//     $(this).css("color", "yellow");
+// });
