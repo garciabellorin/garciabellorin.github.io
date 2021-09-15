83d68b2b9bbe2ccbbe60ee60cbe0e9a9375b59ee
Jose Garcia Bellorin <78644434+Dormeteus@users.noreply.github.com>
2021-09-15 17:55:26 -0500

    Fixing js links

--- fifteen/js/fifteen.js
+++ fifteen/js/fifteen.js
@@ -1,171 +1,168 @@
+window.onload = function () {
+	$("shufflebutton").onclick = shuffle;
+	renderPuzzle();
+};
 
-$(document).ready(function () {
-	"use strict";
-	window.onload = function () {
-		$("shufflebutton").onclick = shuffle;
-		placeElements();
-	};
+//Place the elmemts into right position
+var slideNumber = [];
+function renderPuzzle() {
+	let slide = $$("#puzzlearea div");
+	for (let i = 0; i < slide.length; i++) {
+		slide[i].className = "puzzlepiece";
+		slide[i].id = i;
+		slideNumber[i] = i + 1;
+		moveSlide(slide[i], i);
+		slideBackground(slide[i], i);
+		//Event Handling
+		slide[i].addEventListener('click', movePieceEvent);
+		slide[i].onmouseover = highlightPiece;
+		slide[i].onmouseout = dehighlightPiece;
+	}
+	slideNumber[slide.length] = 0;
+}
 
-	//Place the elmemts into right position
-	var tileNumber = [];
-	function placeElements() {
-		var puzzlepieces = $$("#puzzlearea div");
-		for (let i = 0; i < puzzlepieces.length; i++) {
-			puzzlepieces[i].className = "puzzlepiece";
-			puzzlepieces[i].id = i;
-			tileNumber[i] = i + 1;
-			tilePosition(puzzlepieces[i], i);
-			tileBackground(puzzlepieces[i], i);
+//Place a single piece into right position
+var size = 4;
+function moveSlide(piece, index) {
+	//index -> (x, y)
+	let currX = Math.floor(index / size);
+	let currY = index % size;
 
-			//Event Handling
-			puzzlepieces[i].addEventListener("click", moveTile);
-			puzzlepieces[i].onmouseover = highlightPiece;
-			puzzlepieces[i].onmouseout = dehighlightPiece;
-		}
-		tileNumber[puzzlepieces.length] = 0;
+	let topZero = currX * (400 / size);
+	let sideZero = currY * (400 / size);
+	piece.style.top = topZero + "px";
+	piece.style.left = sideZero + "px";
+}
+
+//Set background images of the pieces
+function slideBackground(piece, index) {
+	var x = Math.floor(index / size);
+	var y = index % size;
+	var fromTheRightEdge = 400 - x * (400 / size);
+	var fromTheBottomEdge = 400 - y * (400 / size);
+	piece.style.backgroundPosition = fromTheBottomEdge + "px " + fromTheRightEdge + "px";
+}
+
+//Move Pieces
+function movePieceEvent(event) {
+	var index = parseInt(this.id);
+	var dest = canMoveTo(index);
+	if (dest != -1) {
+		movePieceFromTo(this, index, dest);
+		//Reset the slideNumber[i]
+		slideNumber[dest] = slideNumber[index];
+		slideNumber[index] = 0;
+		this.id = dest;
 	}
 
-	//Place a single piece into right position
-	var size = 4;
-	function tilePosition(piece, index) {
-		//Position (x, y)
-		var posX = Math.floor(index / size);
-		var posY = index % size;
-
-		var xZeroRef = posX * (400 / size);
-		var yZeroRef = posY * (400 / size);
-		piece.style.top = xZeroRef + "px";
-		piece.style.left = yZeroRef + "px";
-	}
-
-	//Set background images of the pieces
-	function tileBackground(piece, index) {
-		var posX = Math.floor(index / size);
-		var posY = index % size;
-		var xZeroRef = 400 - posX * (400 / size);
-		var yZeroRef = 400 - posY * (400 / size);
-		piece.style.backgroundPosition = yZeroRef + "px " + xZeroRef + "px";
-	}
-
-	//Move Pieces
-	function moveTile(event) {
-		var index = parseInt(this.id);
-		var dest = availablePosition(index);
-		if (dest != -1) {
-			moveTileFromTo(this, index, dest);
-			//Reset the tileNumber[i]
-			tileNumber[dest] = tileNumber[index];
-			tileNumber[index] = 0;
-			this.id = dest;
-		}
-
-		//Check status and change background
-		var puzzlepieces = $$("#puzzlearea div");
-		var correctCount = 0;
-		for (var i = 0; i < puzzlepieces.length; i++) {
-			if (tileNumber[i] == i + 1)
-				correctCount++;
-		}
-		if (tileNumber[puzzlepieces.length] == 0)
+	//Judge whether it is a successful status
+	//If success, change the background image of the body
+	var slide = $$("#puzzlearea div");
+	var correctCount = 0;
+	for (var i = 0; i < slide.length; i++) {
+		if (slideNumber[i] == i + 1)
 			correctCount++;
-		if (correctCount == 16) {
-			$$("body")[0].setStyle({ backgroundColor: "#900" });
+	}
+	if (slideNumber[slide.length] == 0)
+		correctCount++;
+	if (correctCount == 16) {
+		// alert("OH YEAH YOU ARE SUCCESSFUL!");
+		$$('body')[0].setStyle({ backgroundColor: '#900' });
+	}
+	else {
+		$$('body')[0].setStyle({ backgroundColor: 'white' });
+	}
+}
+
+function movePieceFromTo(piece, index, dest) {
+	//Calculation
+	var fromX = Math.floor(index / size) * (400 / size);
+	var fromY = (index % size) * (400 / size);
+	var destX = Math.floor(dest / size) * (400 / size);
+	var destY = (dest % size) * (400 / size);
+	var interval = 10;
+
+	//Begin moving
+	var i = 0;
+	if (fromX == destX) {
+		for (i = 1; i <= 100 / interval; i++)
+			setTimeout(stepMoveTo, i * interval, piece, fromX, fromY + (destY - fromY) / (100 / interval) * i);
+	}
+	else {
+		for (i = 1; i <= 100 / interval; i++)
+			setTimeout(stepMoveTo, i * interval, piece, fromX + (destX - fromX) / (100 / interval) * i, fromY);
+	}
+	//Finish moving
+}
+
+//Caller: movePiece
+function canMoveTo(index) {
+	var destination = null;
+	var left = index - 1;
+	var right = index + 1;
+	var up = index - 4;
+	var down = index + 4;
+
+	if (left >= 0 && left < 16)
+		//if 0 left side and index is not leftmost
+		if (slideNumber[left] == 0 && index % size != 0) {
+			return left;
 		}
-		else {
-			$$("body")[0].setStyle({ backgroundColor: "white" });
+	if (right >= 0 && right < 16)
+		if (slideNumber[right] == 0 && index % size != 3) {
+			return right;
 		}
-	}
-
-	function moveTileFromTo(piece, index, dest) {
-		var fromX = Math.floor(index / size) * (400 / size);
-		var fromY = (index % size) * (400 / size);
-		var destX = Math.floor(dest / size) * (400 / size);
-		var destY = (dest % size) * (400 / size);
-		var interval = 10;
-
-		//Render
-		var i = 0;
-		if (fromX == destX) {
-			for (i = 1; i <= 100 / interval; i++)
-				setTimeout(moveTileTo, i * interval, piece, fromX, fromY + (destY - fromY) / (100 / interval) * i);
+	if (up >= 0 && up < 16)
+		if (slideNumber[up] == 0 && Math.floor(index / size) != 0) {
+			return up;
 		}
-		else {
-			for (i = 1; i <= 100 / interval; i++)
-				setTimeout(moveTileTo, i * interval, piece, fromX + (destX - fromX) / (100 / interval) * i, fromY);
+	if (down >= 0 && down < 16)
+		if (slideNumber[down] == 0 && Math.floor(index / size) != 3) {
+			return down;
 		}
+	return -1;
+}
 
+//Caller: movePiece
+function stepMoveTo(piece, x, y) {
+	piece.style.top = x + "px";
+	piece.style.left = y + "px";
+}
+
+//Highlight and dehighlight pieces
+function highlightPiece(event) {
+	if (canMoveTo(parseInt(this.id)) != -1) {
+		this.style.borderColor = "red";
+		this.style.color = "#006600";
+		this.style.textDecoration = "underline";
 	}
+}
 
-	//Caller: moveTile
-	function availablePosition(index) {
-		var destination = null;
-		var left = index - 1;
-		var right = index + 1;
-		var up = index - 4;
-		var down = index + 4;
+function dehighlightPiece(event) {
+	this.style.borderColor = "black";
+	this.style.color = "black";
+	this.style.textDecoration = "";
+}
 
-		if (left >= 0 && left < 16)
-			//if 0 left side and index is not leftmost
-			if (tileNumber[left] == 0 && index % size != 0) {
-				return left;
-			}
-		if (right >= 0 && right < 16)
-			if (tileNumber[right] == 0 && index % size != 3) {
-				return right;
-			}
-		if (up >= 0 && up < 16)
-			if (tileNumber[up] == 0 && Math.floor(index / size) != 0) {
-				return up;
-			}
-		if (down >= 0 && down < 16)
-			if (tileNumber[down] == 0 && Math.floor(index / size) != 3) {
-				return down;
-			}
-		return -1;
-	}
-
-	//Caller: moveTile
-	function moveTileTo(piece, x, y) {
-		piece.style.top = x + "px";
-		piece.style.left = y + "px";
-	}
-
-	//Highlight and dehighlight pieces
-	function highlightPiece(event) {
-		if (availablePosition(parseInt(this.id)) != -1) {
-			this.style.borderColor = "red";
-			this.style.color = "#006600";
-			this.style.textDecoration = "underline";
+// Shuffle the pieces
+function shuffle(event) {
+	var slide = $$("#puzzlearea div");
+	//use 20 steps to mess up the puzzle
+	for (var step = 0; step < 200; step++) {
+		list = [];
+		//select the movable pieces
+		for (var i = 0; i < slide.length; i++) {
+			var tempIndex = parseInt(slide[i].id);
+			if (canMoveTo(tempIndex) != -1)
+				list.push(slide[i]);
 		}
+		var piece = list[Math.floor(Math.random() * list.length)];
+		var index = parseInt(piece.id);
+		var dest = canMoveTo(index);
+		movePieceFromTo(piece, index, dest);
+		slideNumber[dest] = slideNumber[index];
+		slideNumber[index] = 0;
+		piece.id = dest;
+		list = [];
 	}
-
-	function dehighlightPiece(event) {
-		this.style.borderColor = "black";
-		this.style.color = "black";
-		this.style.textDecoration = "";
-	}
-
-	// Shuffle the pieces
-	function shuffle(event) {
-		var puzzlepieces = $$("#puzzlearea div");
-		//use 20 steps to mess up the puzzle
-		for (var step = 0; step < 200; step++) {
-			list = [];
-			//select the movable pieces
-			for (var i = 0; i < puzzlepieces.length; i++) {
-				var tempIndex = parseInt(puzzlepieces[i].id);
-				if (availablePosition(tempIndex) != -1)
-					list.push(puzzlepieces[i]);
-			}
-			var piece = list[Math.floor(Math.random() * list.length)];
-			var index = parseInt(piece.id);
-			var dest = availablePosition(index);
-			moveTileFromTo(piece, index, dest);
-			tileNumber[dest] = tileNumber[index];
-			tileNumber[index] = 0;
-			piece.id = dest;
-			list = [];
-		}
-
-	}
-});
+}
