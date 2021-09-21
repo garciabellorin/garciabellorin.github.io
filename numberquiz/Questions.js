c8fcdee71ba7c66544caa5140194e5e6ef829de0
Jose Garcia Bellorin <78644434+garciabellorin@users.noreply.github.com>
2021-09-21 10:21:31 -0500

    Add files via upload

---
+++ numberquiz/Questions.js
@@ -0,0 +1,30 @@
+
+const questions=[
+    {
+        "id": 1, 
+        "question": "3, 1, 4, 1, 5",
+        "answer": 9
+    },
+    {
+        "id": 2, 
+        "question": "1, 1, 2, 3, 5",
+        "answer": 8
+    },
+    {
+        "id": 3, 
+        "question": "1, 4, 9, 16, 25",
+        "answer": 36
+    },
+    {
+        "id": 4, 
+        "question": "2, 3, 5, 7, 11",
+        "answer": 13
+    },
+    {
+        "id": 5,
+        "question": "1, 2, 4, 8, 16",
+        "answer": 32
+    },
+];
+
+module.exports = questions;
